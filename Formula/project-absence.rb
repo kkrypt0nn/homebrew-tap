86044cca80e7fe3d7fe339b861ba4f26bccaac04
Krypton <root@krypton.ninja>
2026-04-09 20:41:27 +0200

    feat: project absence formula
    Closes #1.
    
    Signed-off-by: Krypton <root@krypton.ninja>

---
+++ Formula/project-absence.rb
@@ -0,0 +1,19 @@
+class ProjectAbsence < Formula
+  desc "Uncover the unseen"
+  homepage "https://github.com/kkrypt0nn/project-absence"
+  url "https://github.com/kkrypt0nn/project-absence/archive/refs/tags/v0.14.0.tar.gz"
+  sha256 "a9c0698ece59e2fe203a8588f34890d62a1921d6038aa7bc044b36fed3d1b2c7"
+  license "MIT"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", *std_cargo_args
+  end
+
+  test do
+    output = shell_output("#{bin}/project-absence --help")
+    assert_match "Project Absence", output
+    assert_match version.to_s, output
+  end
+end
