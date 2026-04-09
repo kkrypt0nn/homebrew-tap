2d3e93139ca968ef28d0428028afeea84aa757c3
Krypton <root@krypton.ninja>
2026-04-09 19:11:15 +0000

    argane: add 1.0.0 bottle.

--- Formula/argane.rb
+++ Formula/argane.rb
@@ -5,6 +5,12 @@ class Argane < Formula
   sha256 "6e952bcc6e050a67ee81583c49a255e237fa24220b33bb8313daaaba259156cd"
   license "Apache-2.0"
 
+  bottle do
+    root_url "https://github.com/kkrypt0nn/homebrew-tap/releases/download/argane-1.0.0"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:  "eaa18f307fa5ff0b0d7cb0afdf84e77042ad7a558c03d652b90d2a187c861c2f"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "c6c25ce7c4699004faa5cd17bf7c5bce5722a6461b2b014d4b0688dc0e26c452"
+  end
+
   depends_on "go" => :build
 
   def install
