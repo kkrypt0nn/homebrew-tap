0af76fa63af542c17ce800992ef440b6a42896fe
Krypton <root@krypton.ninja>
2026-04-28 17:56:56 +0000

    project-absence: update 1.0.0 bottle.

--- Formula/project-absence.rb
+++ Formula/project-absence.rb
@@ -6,9 +6,9 @@ class ProjectAbsence < Formula
   license "MIT"
 
   bottle do
-    root_url "https://github.com/kkrypt0nn/homebrew-tap/releases/download/project-absence-0.14.0"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:  "0fc7be2d22b2ca21d0bf125c9d920a566551595391ca36d9e0b37a4e269e38d4"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "671eb43451e66db5c29dd163135980fca08aaa90fabbe45b282c40ac9085e0f7"
+    root_url "https://github.com/kkrypt0nn/homebrew-tap/releases/download/project-absence-1.0.0"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:  "167275e553966f8fb0cdcc3c73a87736a41d490d7c17b7e1131d3755a833748b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "8feea36d4630ccad5543181e42a7ae37615288f3f4311e135640ef8086e22968"
   end
 
   depends_on "rust" => :build
